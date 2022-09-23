e3fa907692717cf1927f0e890ee5b2446ad7785f
Füredi Dániel <daniel.furedi03@gmail.com>
2022-09-23 10:17:36 +0200

    első három feladat kész

---
+++ app/src/main/java/hu/home/android/clickcounter/MainActivity.java
@@ -0,0 +1,67 @@
+package hu.home.android.clickcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button hozzaadButton;
+    private Button kivonButton;
+    private TextView szamTextView;
+    private int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        hozzaadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                szamValtozik();
+            }
+        });
+        kivonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                szamValtozik();
+            }
+        });
+        szamTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = 0;
+                szamValtozik();
+            }
+        });
+    }
+
+    private void szamValtozik(){
+        szamTextView.setText(Integer.toString(counter));
+        if (counter >= 1){
+            szamTextView.setTextColor(Color.parseColor("#09fb0e"));
+        } else if (counter == 0){
+            szamTextView.setTextColor(Color.parseColor("#0000fe"));
+
+        }else {
+            szamTextView.setTextColor(Color.parseColor("#ff0000"));
+        }
+    }
+
+    private void init(){
+        hozzaadButton = findViewById(R.id.hozzaadButton);
+        kivonButton = findViewById(R.id.kivonButton);
+        szamTextView = findViewById(R.id.szamTextView);
+        counter = 0;
+    }
+}
